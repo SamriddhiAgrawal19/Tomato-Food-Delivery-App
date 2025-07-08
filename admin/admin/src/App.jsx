74c51af134edb0086a433e07c57509a04fee5377
Samriddhi Agrawal <138271207+SamriddhiAgrawal19@users.noreply.github.com>
2025-07-09 00:16:05 +0530

    Update App.jsx

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://tomato-food-delivery-app-7l8p.onrender.com";
   return (
    
     <div>
