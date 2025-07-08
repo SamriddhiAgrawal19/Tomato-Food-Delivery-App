59623ca2cef8d90f5f787700ac3752d2ca2b2e45
Samriddhi Agrawal <138271207+SamriddhiAgrawal19@users.noreply.github.com>
2025-07-09 00:05:19 +0530

    Update orderController.js

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -6,7 +6,7 @@ import 'dotenv/config';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://tomato-food-delivery-app-frontend-dr4b.onrender.com/cart";
 
     try {
         
