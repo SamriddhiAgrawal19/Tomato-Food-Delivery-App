df1c271885eadb214da4c089ba8775545a891376
Samriddhi Agrawal <138271207+SamriddhiAgrawal19@users.noreply.github.com>
2025-07-11 13:12:48 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://acsamriddhi05:XkjCoaH4R3EhLY1q@cluster0.btfogcu.mongodb.net/food-del")
+    await mongoose.connect("mongodb+srv://acsamriddhi05:fbP9juaFuzDZ1yIi@cluster0.btfogcu.mongodb.net/food-del")
   .then(() => {
     console.log("Connected to MongoDB");
   })
